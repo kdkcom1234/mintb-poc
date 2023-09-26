a2d3fd193ff700a39c1dceed5d7464dfb8629a72
Daekeun Ko <kdkcom1234@gmail.com>
2023-09-27 01:04:10 +0900

    설명 수정

--- mintb-poc-app/lib/main.dart
+++ mintb-poc-app/lib/main.dart
@@ -42,9 +42,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TodoList with Navigation',
+      title: 'MintB POC App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       initialRoute: getUid() == "" ? "/sign-in" : "/",
