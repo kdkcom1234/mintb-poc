398df074130f645f8d4fcd439ad0dfa8b3465074
Daekeun Ko <kdkcom1234@gmail.com>
2023-10-11 17:36:46 +0900

    밸런스 조회 추가

---
+++ mintb-poc-app/lib/chain/balance.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart' as http;
+import 'package:web3dart/web3dart.dart';
+
+import 'mintb_poc_token.dart';
+
+Future<void> getBalance() async {
+  final client = Web3Client(
+    'https://data-seed-prebsc-1-s1.binance.org:8545/', // Binance Smart Chain 테스트넷 URL
+    http.Client(),
+  );
+
+  final privateKey =
+      EthPrivateKey.fromHex('YOUR_PRIVATE_KEY_HERE'); // 본인의 Private Key로 교체
+  final address = privateKey.address;
+
+  // 네이티브 코인 밸런스 조회
+  EtherAmount nativeBalance = await client.getBalance(address);
+
+  print('BNB Balance: ${nativeBalance.getInEther}');
+
+  // BEP-20 토큰 밸런스 조회
+  final tokenContractAddress = EthereumAddress.fromHex(mintbPocTokenAddress);
+  final tokenContract = DeployedContract(
+      ContractAbi.fromJson(mintbPocTokenAPI, 'BEP20Token'),
+      tokenContractAddress);
+
+  final balanceFunction = tokenContract.function('balanceOf');
+  final tokenBalance = await client.call(
+      contract: tokenContract, function: balanceFunction, params: [address]);
+
+  print('BEP-20 Token Balance: ${tokenBalance.first}');
+}
