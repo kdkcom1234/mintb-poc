8b5f312711971b318766f09d316881fc147dbcfe
Daekeun Ko <kdkcom1234@gmail.com>
2023-11-03 14:23:03 +0900

    임시

--- mintb-poc-app/lib/constants.dart
+++ mintb-poc-app/lib/constants.dart
@@ -3,6 +3,6 @@ const isLocalDevelopment = true;
 // 여자: Otoo
 // const localUser = {"mintbfemale@gmail.com", "password1234"};
 // 남자: V
-// const localUser = {"kdkcom1234@gmail.com", "password1234"};
-// 남자: Son
-const localUser = {"kdkcom@naver.com", "password1234"};
+const localUser = {"kdkcom1234@gmail.com", "password1234"};
+//남자: Son
+// const localUser = {"kdkcom@naver.com", "password1234"};
