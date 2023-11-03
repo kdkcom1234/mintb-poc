6e6363441cca7217d07a7e0dca7b92984dadc34c
Daekeun Ko <kdkcom1234@gmail.com>
2023-11-03 11:40:37 +0900

    로직 수정

--- mintb-poc-firebase/functions/src/modules/triggers/auctionBidRequests.ts
+++ mintb-poc-firebase/functions/src/modules/triggers/auctionBidRequests.ts
@@ -22,36 +22,48 @@ export const auctionBidRequestsCreated = onDocumentCreated(
 
       const amount = data["amount"] as number;
 
+      // TODO: 이전 최상위 입찰보다 amount가 커야되는 로직 추가해야함
+
+      // 포인트가 존재하고, 입찰수량이 보유포인트 보다 작은지 체크
       const doc = await db.doc(`/points/${event.params.uid}`).get();
       const pointData = doc.data();
-      if (pointData) {
+      let point = 0;
+
+      if (!pointData) {
+        return; // confirm: false
+      } else {
         logger.log("pointData: ", JSON.stringify(pointData));
 
-        const point = pointData["mint"] as number;
+        point = pointData["mint"] as number;
         logger.log(`amount: ${amount} point: ${point}`);
 
-        if (point >= amount) {
-          // 입찰 등록
-          const bidDocRef =
-            db.doc("/auctions/" +
-              `${event.params.auctionId}/bids/${event.params.uid}`);
-          await bidDocRef.set({
-            amount,
-            createdAt: FieldValue.serverTimestamp(),
-          });
-
-          // 포인트 차감
-          const decreasedPoint = point - amount;
-          const pointDocRef = db.doc(`/points/${event.params.uid}`);
-          await pointDocRef.set({ mint: decreasedPoint });
-
-          // 입찰 요청정보 삭제
-          const bidReqDocRef =
-            db.doc("/auctions" +
-              `/${event.params.auctionId}/bid-requests/${event.params.uid}`);
-          await bidReqDocRef.delete();
+        // 입찰수량이 보유 포인트 보다 크면 종료
+        if (amount > point) {
+          return; // confirm: false
         }
       }
+
+      // 입찰 등록
+      const bidDocRef =
+        db.doc("/auctions/" +
+          `${event.params.auctionId}/bids/${event.params.uid}`);
+      await bidDocRef.set({
+        amount,
+        createdAt: FieldValue.serverTimestamp(),
+      });
+
+      // 포인트 차감
+      const decreasedPoint = point - amount;
+      const pointDocRef = db.doc(`/points/${event.params.uid}`);
+      await pointDocRef.set({ mint: decreasedPoint });
+
+      // 입찰 요청정보 삭제(임시)
+      // TODO: 입찰이 완료되면 요청정보는 confirm = true로 바뀜
+      // TODO: rules에 allow create: if request.resource.confirm == false 로 초기생성에는 false로 시작
+      const bidReqDocRef =
+        db.doc("/auctions" +
+          `/${event.params.auctionId}/bid-requests/${event.params.uid}`);
+      await bidReqDocRef.delete();
     }
   }
 );
